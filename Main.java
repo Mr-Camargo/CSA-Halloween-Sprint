f78a01c4b26c69583cb54c64260da89147dde497
Sebastián Camargo Marín <sebastian@camargomar.in>
2025-10-14 12:43:38 -0400

    Unification of Files Under One Main Class
    Let's hope this doesn't explode

---
+++ Main.java
@@ -0,0 +1,56 @@
+class Room {
+    String name;
+    String description;
+    ArrayList<Item> items = new ArrayList<>();
+    ArrayList<Room> exits = new ArrayList<>();
+    
+    public void addExit(Room room) {
+        exits.add(room);
+    }
+    
+    public void addItem(Item item) {
+        items.add(item);
+    }
+    
+    public void enterRoom() {
+        System.out.println("You are in " + name);
+        System.out.println(description);
+        System.out.println("Items in the room: " + items);
+        System.out.println("Exits: " + exits);
+    }
+}
+
+class Item {
+    String name;
+    String description;
+    String effect;
+
+    public void use(Player player, Monster monster) {
+        // Define what the item does
+    }
+}
+
+class Monster {
+    String name;
+    int strength;
+    String description;
+
+    public void attack(Player player) {
+        // Reduce player’s health based on monster’s strength
+    }
+}
+
+class Player {
+    int health = 100;
+    ArrayList<Item> inventory = new ArrayList<>();
+    Room currentRoom;
+
+    public void pickUpItem(Item item) {
+        inventory.add(item);
+    }
+
+    public void move(Room nextRoom) {
+        currentRoom = nextRoom;
+        currentRoom.enterRoom();
+    }
+}
